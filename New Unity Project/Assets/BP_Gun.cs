48f4c6427ed5dc5620deda688f10e240bb6f8846
Joseph Adams <WASDMagician@gmail.com>
2017-10-24 14:49:38 +0100

    .

--- New Unity Project/Assets/BP_Gun.cs
+++ New Unity Project/Assets/BP_Gun.cs
@@ -6,6 +6,7 @@ public class BP_Gun: MonoBehaviour {
 
     public GameObject point_at;
     public GameObject firing_point;
+    public Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
@@ -15,16 +16,17 @@ public class BP_Gun: MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //this.transform.LookAt(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)));
+        Ray2D ray = new Ray2D(firing_point.transform.position, firing_point.transform.forward);
+        Debug.DrawRay(firing_point.transform.position, offset + firing_point.transform.forward * 10);
         this.transform.LookAt(point_at.transform.position);
         if(Input.GetMouseButtonUp(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(firing_point.transform.position, firing_point.transform.forward);
+            RaycastHit2D hit = Physics2D.Raycast(firing_point.transform.position, firing_point.transform.position + firing_point.transform.forward + offset);
             if(hit.collider != null)
             {
                 BP_Balloon_Hit hit_controller = hit.collider.gameObject.GetComponent<BP_Balloon_Hit>();
                 if(hit_controller != null)
                 {
-                    print("HIT OBJECt");
                     hit_controller.On_Hit();
                 }
             }
--- New Unity Project/Assets/Games/All/Scripts/Draw_Gizmos.cs
+++ New Unity Project/Assets/Games/All/Scripts/Draw_Gizmos.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Draw_Gizmos : MonoBehaviour {
 
     public bool draw_gizmos;
+    public Vector3 size;
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
         if (draw_gizmos == true)
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawCube(this.transform.position, Vector3.one);
+            Gizmos.DrawCube(this.transform.position, size);
         }
     }
 #endif
